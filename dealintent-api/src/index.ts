b539a9575a5785c8a4b553eda7f719ea268feeb3
aradhyap <manuaradhya07@gmail.com>
2023-11-07 00:05:06 +0530

    cors, ui boilerplate cleanup

--- dealintent-api/src/index.ts
+++ dealintent-api/src/index.ts
@@ -10,6 +10,13 @@ config();
 const port = parseInt(GetEnv('PORT')) || 3001;
 const app = express();
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+  next();
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+});
 initialize();
 
 app.listen(port, () =>
---
+++ dealintent-ui/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="container mx-auto border-2 p-3 bg-white shadow-sm rounded-md my-2 sticky">
+      <h2 className="text-xl font-bold text-center text-indigo-700">Kanban Board</h2>
+    </div>
+  )
+}
+
+export default Navbar
