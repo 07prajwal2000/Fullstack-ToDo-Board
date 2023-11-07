a4096078f9333a4809994166c6544c12d6e53cd3
aradhyap <manuaradhya07@gmail.com>
2023-11-08 00:47:14 +0530

    CORS endpoint to all

--- dealintent-api/src/index.ts
+++ dealintent-api/src/index.ts
@@ -11,7 +11,7 @@ const port = parseInt(GetEnv('PORT')) || 3001;
 const app = express();
 app.use(bodyParser.json())
 app.use((_, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
